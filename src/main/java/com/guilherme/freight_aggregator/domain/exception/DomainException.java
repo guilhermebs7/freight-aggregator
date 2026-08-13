f9e2c97b1f78e816f2686956e2be1cf5e20d69b5
guilhermebs7 <gbs5@cin.ufpe.br>
2026-08-13 18:15:12 -0300

    feat(exception): add DomainException class for handling domain-specific errors

---
+++ src/main/java/com/guilherme/freight_aggregator/domain/exception/DomainException.java
@@ -0,0 +1,7 @@
+package com.guilherme.freight_aggregator.domain.exception;
+
+public class DomainException  extends RuntimeException{
+    public DomainException(String message){
+        super(message);
+    }
+}
