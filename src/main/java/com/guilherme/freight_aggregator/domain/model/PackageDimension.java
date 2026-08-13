0c6bac627fb46ab41e7f8c62b87c5a8dca0f8738
guilhermebs7 <gbs5@cin.ufpe.br>
2026-08-13 17:06:33 -0300

    feat(shipping): add PackageDimension record with validation and weight calculation

---
+++ src/main/java/com/guilherme/freight_aggregator/domain/model/PackageDimension.java
@@ -0,0 +1,18 @@
+package com.guilherme.freight_aggregator.domain.model;
+
+public record PackageDimension(double larguraCm,double alturaCm, double comprimentoCm,double pesoKg) {
+
+    public PackageDimension{
+        if (larguraCm <=0 || alturaCm <=0 || comprimentoCm <=0 || pesoKg <=0){
+            throw new IllegalArgumentException("Dimensões e peso devem ser maiores que zero");
+        }
+    }
+
+    public double obterPesoCubico(){                                      // Cálculo do peso cubado( Fator de cubagem padrão: 6000)
+        return (larguraCm * alturaCm * comprimentoCm) / 6000.0;
+    }
+
+    public double obterPesoEfetivo(){
+        return Math.max(pesoKg, obterPesoEfetivo());
+    }
+}
