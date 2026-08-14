4053327d7d70c22b67ba3a8cd7a2cb2d5987740c
guilhermebs7 <gbs5@cin.ufpe.br>
2026-08-14 16:49:32 -0300

    feat(freight): implement CorreiosAdapter for carrier integration

---
+++ src/main/java/com/guilherme/freight_aggregator/infrastructure/adapters/out/carriers/CorreiosAdapter.java
@@ -0,0 +1,27 @@
+package com.guilherme.freight_aggregator.infrastructure.adapters.out.carriers;
+
+import com.guilherme.freight_aggregator.application.ports.out.CarrierIntegrationPort;
+import com.guilherme.freight_aggregator.domain.model.Address;
+import com.guilherme.freight_aggregator.domain.model.FreightOption;
+import com.guilherme.freight_aggregator.domain.model.PackageDimension;
+
+import java.util.Optional;
+
+public class CorreiosAdapter implements CarrierIntegrationPort {
+    @Override
+    public Optional<FreightOption> calcularTaxa(Address origem, Address destino, PackageDimension dimensaoPacote) {
+        try {
+            // aqui seria feita a chamada HTTP para API dos Correios.
+            // Simulação de cálculo baseado no peso efeitivo
+            double preco= 20.0 + (dimensaoPacote.obterPesoEfetivo() * 4.5);
+            int diaEntrega=5;
+
+             return Optional.of(new FreightOption("Correios", "SEDEX",preco,diaEntrega));
+
+        }catch (Exception e){
+
+            //em caso de falha na API do parceiro, retornamos Optional.empty() sem quebrar o sistema
+            return Optional.empty();
+        }
+    }
+}
