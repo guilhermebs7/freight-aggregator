2c8074b9ec844961fe88376b1c7d4ac5e02128a1
guilhermebs7 <gbs5@cin.ufpe.br>
2026-08-14 15:17:57 -0300

    feat(freight): add CarrierIntegrationPort interface for carrier integration

---
+++ src/main/java/com/guilherme/freight_aggregator/application/ports/in/CarrierIntegrationPort.java
@@ -0,0 +1,13 @@
+package com.guilherme.freight_aggregator.application.ports.in;
+
+import com.guilherme.freight_aggregator.domain.model.Address;
+import com.guilherme.freight_aggregator.domain.model.FreightOption;
+import com.guilherme.freight_aggregator.domain.model.PackageDimension;
+
+import java.util.Optional;
+
+public interface CarrierIntegrationPort {
+
+    Optional<FreightOption> calcularTaxa(Address origem, Address destino, PackageDimension dimensaoPacote);  // calcula a taxa de entrega de acordo com as oções de transportadora
+
+}
